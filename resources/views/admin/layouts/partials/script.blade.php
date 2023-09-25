c2a96d80dd79dbfc38560499b4f0b1834decea8c
duong8888 <duongna16032003@gmail.com>
2023-09-25 23:17:04 +0700

    no commit

--- resources/views/admin/layouts/partials/script.blade.php
+++ resources/views/admin/layouts/partials/script.blade.php
@@ -1,14 +1,14 @@
 <!-- Vendor -->
-<script src="admin/libs/jquery/jquery.min.js"></script>
-<script src="admin/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="admin/libs/simplebar/simplebar.min.js"></script>
-<script src="admin/libs/node-waves/waves.min.js"></script>
-<script src="admin/libs/waypoints/lib/jquery.waypoints.min.js"></script>
-<script src="admin/libs/jquery.counterup/jquery.counterup.min.js"></script>
-<script src="admin/libs/feather-icons/feather.min.js"></script>
+<script src="{{asset('admin/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('admin/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('admin/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('admin/libs/node-waves/waves.min.js')}}"></script>
+<script src="{{asset('admin/libs/waypoints/lib/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('admin/libs/jquery.counterup/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('admin/libs/feather-icons/feather.min.js')}}"></script>
 
 <!-- knob plugin -->
-<script src="admin/libs/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{asset('admin/libs/jquery-knob/jquery.knob.min.js')}}"></script>
 
 <!-- App js-->
-<script src="admin/js/app.min.js"></script>
+<script src="{{asset('admin/js/app.min.js')}}"></script>
