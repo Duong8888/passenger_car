5049f58682771d3b94bb1b3123f583e9569ae222
duong8888 <duongna16032003@gmail.com>
2023-09-25 08:09:13 +0700

    first commit

---
+++ resources/views/admin/layouts/partials/style.blade.php
@@ -0,0 +1,8 @@
+<!-- App favicon -->
+<link rel="shortcut icon" href="admin/images/favicon.ico">
+
+<!-- App css -->
+<link href="admin/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+<!-- icons -->
+<link href="admin/css/icons.min.css" rel="stylesheet" type="text/css" />
