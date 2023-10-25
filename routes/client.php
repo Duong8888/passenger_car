84eadb9ad7312e4e075880540f458a151da93c35
nguyenngochoa03 <ngochoa032111@gmail.com>
2023-10-26 00:41:07 +0700

    update

--- routes/client.php
+++ routes/client.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Client\BlogController;
 use App\Http\Controllers\Client\CategoryController;
 use App\Http\Controllers\Client\ProfileController;
-use App\Http\Controllers\PhoneAuthController;
+//use App\Http\Controllers\PhoneAuthController;
 
 //Nam
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -35,6 +35,7 @@ Route::group(['prefix' => 'notifications', 'as' => 'notifications.'], function (
 
 Route::get('/blog/{id}', [BlogController::class, 'blog'])->name('blog');
 Route::get('/blogs', [BlogController::class, 'show'])->name('blog.show');
+Route::get('/category-detail/{id}',[BlogController::class, 'show'])->name('category-detail');
 
 
 Route::post('/update-ticket', [TicketController::class, 'CountTicket'])->name('client.ticket.update-ticket');
