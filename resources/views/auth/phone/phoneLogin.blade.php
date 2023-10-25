98260e6d29fc7e30541870b746b2d3820727dd12
nguyenngochoa03 <ngochoa032111@gmail.com>
2023-10-26 00:41:30 +0700

    Update phoneLogin.blade.php

--- resources/views/auth/phone/phoneLogin.blade.php
+++ resources/views/auth/phone/phoneLogin.blade.php
@@ -12,6 +12,7 @@
         // messagingSenderId: "793008679902",
         // appId: "1:793008679902:web:01e40657ee99f0150dcb4f",
         // measurementId: "G-CTLCZLM3R1"
+
         // apiKey: "AIzaSyAohN1P7SwBHB0BCSz06qez3Hk3AgE3WQM",
         // authDomain: "timxekhach-44f63.firebaseapp.com",
         // projectId: "timxekhach-44f63",
@@ -19,21 +20,22 @@
         // messagingSenderId: "460281893369",
         // appId: "1:460281893369:web:ac713dd4617600d5716d28",
         // measurementId: "G-JZQDVWF1MB"
-        apiKey: "AIzaSyB2CqirEwrZeVC6YKIHitaIHCxLHygOlAs",
-        authDomain: "fir-6cd66.firebaseapp.com",
-        databaseURL: "https://fir-6cd66-default-rtdb.firebaseio.com",
-        projectId: "fir-6cd66",
-        storageBucket: "fir-6cd66.appspot.com",
-        messagingSenderId: "167315184992",
-        appId: "1:167315184992:web:9bfc9570f1fd3179611205",
-        measurementId: "G-0195R4LR4V"
-        // apiKey: "AIzaSyCJ8pbe36jbzUmVQK_pFOZlPKXRW6JNoG8",
-        // authDomain: "test2-5f15d.firebaseapp.com",
-        // projectId: "test2-5f15d",
-        // storageBucket: "test2-5f15d.appspot.com",
-        // messagingSenderId: "660182456617",
-        // appId: "1:660182456617:web:89d3c4ddf2b96307efff38",
-        // measurementId: "G-31DTR2L4VF"
+
+        // apiKey: "AIzaSyB2CqirEwrZeVC6YKIHitaIHCxLHygOlAs",
+        // authDomain: "fir-6cd66.firebaseapp.com",
+        // databaseURL: "https://fir-6cd66-default-rtdb.firebaseio.com",
+        // projectId: "fir-6cd66",
+        // storageBucket: "fir-6cd66.appspot.com",
+        // messagingSenderId: "167315184992",
+        // appId: "1:167315184992:web:9bfc9570f1fd3179611205",
+        // measurementId: "G-0195R4LR4V"
+        apiKey: "AIzaSyCJ8pbe36jbzUmVQK_pFOZlPKXRW6JNoG8",
+        authDomain: "test2-5f15d.firebaseapp.com",
+        projectId: "test2-5f15d",
+        storageBucket: "test2-5f15d.appspot.com",
+        messagingSenderId: "660182456617",
+        appId: "1:660182456617:web:89d3c4ddf2b96307efff38",
+        measurementId: "G-31DTR2L4VF"
     };
     firebase.initializeApp(firebaseConfig);
 </script>
@@ -49,7 +51,7 @@
 
     function checkEnter(event) {
         if (event.key === "Enter") {
-            verify(); 
+            verify();
         }
     }
 
@@ -68,7 +70,7 @@
             $("#successAuth").text("Message sent");
             $("#successAuth").show();
             localStorage.setItem("phoneNumber", number);
-            window.location.href = "/verify-otp?verificationId=" + confirmationResult.verificationId; 
+            window.location.href = "/verify-otp?verificationId=" + confirmationResult.verificationId;
         }).catch(function (error) {
             $("#error").text(error.message);
             $("#error").show();
@@ -78,14 +80,14 @@
     function verify() {
         var code = $("#verificationId").val();
         var phoneNumber = localStorage.getItem("phoneNumber");
-        
+
         const urlParams = new URLSearchParams(window.location.search);
         const verificationId = urlParams.get("verificationId");
 
         const confirmationResult = firebase.auth.PhoneAuthProvider.credential(verificationId, code);
 
         firebase.auth().signInWithCredential(confirmationResult).then(function (result) {
-          
+
                 $.ajax({
                     type: 'POST',
                     url: '/process',
