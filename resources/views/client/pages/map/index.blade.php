994060bb45dacee6d062dfda6847362d235b70ee
nguyenngochoa03 <ngochoa032111@gmail.com>
2023-10-27 00:22:17 +0700

    add map view function

--- resources/views/client/pages/map/index.blade.php
+++ resources/views/client/pages/map/index.blade.php
@@ -6,11 +6,6 @@
         <div class="container mx-auto">
             <div
                 class="relative mt-4 overflow-hidden transition-all duration-500 ease-in-out bg-white border rounded-md border-gray-100/50 group/jobs group-data-[theme-color=violet]:hover:border-violet-500 group-data-[theme-color=sky]:hover:border-sky-500 group-data-[theme-color=red]:hover:border-red-500 group-data-[theme-color=green]:hover:border-green-500 group-data-[theme-color=pink]:hover:border-pink-500 group-data-[theme-color=blue]:hover:border-blue-500 hover:-translate-y-2 dark:bg-neutral-900 dark:border-neutral-600 ">
-                <div
-                    class="w-48 absolute -top-[5px] -left-20 -rotate-45 group-data-[theme-color=violet]:bg-violet-500/20 group-data-[theme-color=sky]:bg-sky-500/20 group-data-[theme-color=red]:bg-red-500/20 group-data-[theme-color=green]:bg-green-500/20 group-data-[theme-color=pink]:bg-pink-500/20 group-data-[theme-color=blue]:bg-blue-500/20 group-data-[theme-color=violet]:group-hover/jobs:bg-violet-500 group-data-[theme-color=sky]:group-hover/jobs:bg-sky-500 group-data-[theme-color=red]:group-hover/jobs:bg-red-500 group-data-[theme-color=green]:group-hover/jobs:bg-green-500 group-data-[theme-color=pink]:group-hover/jobs:bg-pink-500 group-data-[theme-color=blue]:group-hover/jobs:bg-blue-500 transition-all duration-500 ease-in-out p-[6px] text-center dark:bg-violet-500/20">
-                    <a href="javascript:void(0)" class="text-2xl text-white align-middle"><i
-                            class="mdi mdi-star"></i></a>
-                </div>
                 <div class="p-4">
                     <div class="grid items-center grid-cols-12">
                         <div class="col-span-12 lg:col-span-2">
@@ -22,53 +17,48 @@
                             </div>
                         </div>
                         <!--end col-->
-                        <div class="col-span-12 lg:col-span-3">
-                            <div class="mb-2 mb-md-0">
-                                <h5 class="mb-1 fs-18">
-                                    <a href="job-details.html"
-                                       id="license_plate"
-                                       class="text-gray-900 dark:text-gray-50">Tên
-                                        nhà xe</a>
-                                </h5>
-                                <p class="mb-0 text-gray-500 fs-14 dark:text-gray-300">Ghế ngồi
-                                    45 chỗ</p>
-                            </div>
-                        </div>
-                        <!--end col-->
-                        <div class="col-span-12 lg:col-span-3">
-                            <div class="mb-2 lg:flex">
-                                <div class="flex-shrink-0">
-                                    <i style="font-size: 11px;padding-left: 2px"
-                                       class="mr-1 group-data-[theme-color=violet]:text-violet-500 group-data-[theme-color=sky]:text-sky-500 group-data-[theme-color=red]:text-red-500 group-data-[theme-color=green]:text-green-500 group-data-[theme-color=pink]:text-pink-500 group-data-[theme-color=blue]:text-blue-500 fa-solid fa-circle-dot"></i>
-                                </div>
-                                <p class="mb-0 text-gray-500 dark:text-gray-300"><span style="font-weight: bold;"> 00:00 -- 03:00</span>
-                                </p>
-                            </div>
-                            <div class="mb-2 lg:flex">
-                                <div class="flex-shrink-0">
-                                    <i style="font-size: 11px;padding-left: 2px"
-                                       class="mr-1 group-data-[theme-color=violet]:text-violet-500 group-data-[theme-color=sky]:text-sky-500 group-data-[theme-color=red]:text-red-500 group-data-[theme-color=green]:text-green-500 group-data-[theme-color=pink]:text-pink-500 group-data-[theme-color=blue]:text-blue-500 fa-solid fa-circle-dot"></i>
-                                </div>
-                                <p class="mb-0 text-gray-500 dark:text-gray-300"><span style="font-weight: bold;"> 19:01 -- 23:59</span>
-                                </p>
-                            </div>
-                        </div>
-
-                        <!--end col-->
                         <div class="col-span-12 lg:col-span-2">
-                            <div>
-                                <div class="mb-2 lg:flex" style="margin-left: -35px">
-                                    <div class="flex-shrink-0">
-                                        <i class="mr-1 group-data-[theme-color=violet]:text-violet-500 group-data-[theme-color=sky]:text-sky-500 group-data-[theme-color=red]:text-red-500 group-data-[theme-color=green]:text-green-500 group-data-[theme-color=pink]:text-pink-500 group-data-[theme-color=blue]:text-blue-500 mdi mdi-map-marker"></i>
-                                    </div>
-                                    <p class="mb-0 text-gray-500 dark:text-gray-300" style="font-weight: bold;"><span
-                                            style="font-weight: bold;">Hà Nội</span>
-                                        --&gt; Nghệ An</p>
-                                </div>
-                                <p style=" font-weight: bold;" class="mb-2 text-gray-500 dark:text-gray-300"><span
-                                        style="color: #1890ff;">200,000đ</span>
+                            <div class="mb-2 mb-md-0">
+                                <p class="mb-0 text-gray-500 dark:text-gray-300">
+                                    <span>
+                                        Mã vé:
+                                        <span id="ticketId"></span>
+                                    </span>
                                 </p>
+                                <p class="mb-0 text-gray-500 dark:text-gray-300">
+                                    <span>Ghế ngồi 45 chỗ</span>
+                                </p>
+                            </div>
+                        </div>
+                        <!--end col-->
+                        <div class="col-span-12 lg:col-span-3">
+                            <div>
+                                <p class="mb-0 text-gray-500 dark:text-gray-300">
+                                    <span>
+                                        Giờ Đón:
+                                    <span id="departureTime"></span>
+                                </span>
+                                </p>
+                                <p class="mb-0 text-gray-500 dark:text-gray-300">
+                                    <span>
+                                        Giờ Trả:
+                                    <span id="arrivalTime"></span>
+                                </span>
+                                </p>
+                            </div>
+                        </div>
 
+                        <!--end col-->
+                        <div class="col-span-12 lg:col-span-5">
+                            <div>
+                                <p class="mb-0 text-gray-500 dark:text-gray-300" >
+                                    <span>Điểm Đón: </span>
+                                    <span id="departure"></span>
+                                </p>
+                                <p class="mb-0 text-gray-500 dark:text-gray-300" >
+                                    <span>Điểm Trả: </span>
+                                    <span id="arrival"></span>
+                                </p>
                             </div>
                         </div>
                         <!--end col-->
@@ -79,64 +69,195 @@
                     <div class="grid grid-cols-12">
                         <div class="col-span-12 lg:col-span-4">
                             <div>
-                                <p class="mb-0 text-gray-500 dark:text-gray-300"><span
-                                        id="license_plate"
-                                        class="text-gray-900 dark:text-gray-50">Biển số xe :</span> B001
+                                <p class="mb-0 text-gray-500 dark:text-gray-300">
+                                    <span
+                                        class="text-gray-900 dark:text-gray-50">Biển số xe :</span>
+                                    <span id="licensePlate"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end row-->
+                </div>
+            </div>
+            <div
+                class="relative mt-4 overflow-hidden transition-all duration-500 ease-in-out bg-white border rounded-md border-gray-100/50 group/jobs group-data-[theme-color=violet]:hover:border-violet-500 group-data-[theme-color=sky]:hover:border-sky-500 group-data-[theme-color=red]:hover:border-red-500 group-data-[theme-color=green]:hover:border-green-500 group-data-[theme-color=pink]:hover:border-pink-500 group-data-[theme-color=blue]:hover:border-blue-500 hover:-translate-y-2 dark:bg-neutral-900 dark:border-neutral-600 ">
+                <div class="p-4">
+                    <div class="grid items-center grid-cols-12">
+                        <div class="col-span-12 lg:col-span-3">
+                            <div class="mb-2 mb-md-0">
+                                <p class="mb-0 text-gray-500 fs-14 dark:text-gray-300">
+                                    Khách hàng :
+                                    <span id="username"></span>
+                                </p>
+                                <p class="mb-0 text-gray-500 fs-14 dark:text-gray-300">Liên hệ :
+                                    <span id="phone"></span>
                                 </p>
                             </div>
                         </div>
                         <!--end col-->
-                        <div class="col-span-12 lg:col-span-6">
+                        <div class="col-span-12 lg:col-span-3">
+                            <div class="mb-2 mb-md-0">
+                                <p class="mb-0 text-gray-500 fs-14 dark:text-gray-300">
+                                    Giờ đặt :
+                                    <span id="createdAt"></span>
+                                </p>
 
-                        </div>
-                        <!--end col-->
-                        <div class="col-span-3 lg:col-span-2">
-                            <div class="text-start text-md-end dark:text-gray-50">
-                                <form action="http://127.0.0.1:8000/passengerCar-detail" method="POST">
-                                    <input type="hidden" name="_token" value="SadUIkXEvaei9BduGEZeXFlXaAwuxyOlk54uZr3c"
-                                           autocomplete="off"> <input type="text" hidden="" name="passenger_id"
-                                                                      value="1">
-                                    <input type="text" hidden="" name="image_id" value="">
-                                    <input type="text" hidden="" name="route_id" value="1">
-                                    <button fdprocessedid="3icpir"><i class="mdi mdi-chevron-double-right"></i>Đặt vé
-                                        ngay
-                                    </button>
-                                </form>
-
+                                <p class="mb-2 text-gray-500 dark:text-gray-300">
+                                    Giá Vé:
+                                    <span
+                                        id="price"
+                                        style="color: #1890ff;"></span>
+                                </p>
                             </div>
                         </div>
+
                         <!--end col-->
+                        <div class="col-span-12 lg:col-span-4">
+                            <div>
+                                <p class="mb-0 text-gray-500 dark:text-gray-300">
+                                    Trạng Thái:
+                                    <span
+                                        id="paymentMethod"
+                                        style="color: #1890ff;"></span>
+                                </p>
+                                <p class="mb-2 text-gray-500 dark:text-gray-300">Tổng Tiền :
+                                    <span id="totalPrice" style="color: #1890ff;"></span>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!--end row-->
+                </div>
+                <div class="p-3 bg-gray-50 ">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-12 lg:col-span-4">
+                            <div>
+                                <p class="mb-0">
+                                    <span class="">Bản Đồ</span>
+                                </p>
+                            </div>
+                            <div id="map_canvas" style="width:100vw; height:50vh"></div>
+                        </div>
                     </div>
                     <!--end row-->
                 </div>
             </div>
         </div>
     </section>
-
     <script src="{{asset('admin/libs/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('admin/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW0AbPAlmKNN8x1cc89jQXkhNHlRQnZ_M"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             console.log("ready!");
+            let startLatLng = [35.712408, 139.776188]; // Ga Ueno
+            let targetLatLng = [35.710552, 139.777074]; // LIG
+            let goalMarkerImg = 'https://test.l-svr.net/blog/images/marker-on-dummy.png';
+            let map;
+
             $.ajax({
                 url: "/map",
                 data: {
                     ticketId: '{{$ticketId}}'
                 },
                 success: function (result) {
-                    drawCarProcess(result);
+                    drawCarProcess(result.data);
                 }
             });
 
             function drawCarProcess(data) {
-                if(data && data.length > 0){
+                if (data && data.length) {
                     let ticketDetail = data[0]
-                    $('#license_plate').val(ticketDetail['license_plate']);
-                    $('#departure_time').val(ticketDetail['departure_time']);
-                    $('#arrival_time').val(ticketDetail['arrival_time']);
-                    $('#phone').val(ticketDetail['phone']);
-
+                    $('#licensePlate').text(ticketDetail['license_plate']);
+                    $('#departureTime').text(ticketDetail['departure_time']);
+                    $('#arrivalTime').text(ticketDetail['arrival_time']);
+                    $('#departure').text(ticketDetail['departure']);
+                    $('#arrival').text(ticketDetail['arrival']);
+                    $('#price').text(ticketDetail['price']);
+                    $('#username').text(ticketDetail['username']);
+                    $('#phone').text(ticketDetail['phone']);
+                    $('#createdAt').text(ticketDetail['created_at']);
+                    $('#totalPrice').text(ticketDetail['total_price']);
+                    $('#paymentMethod').text(ticketDetail['payment_method']);
+                    $('#ticketId').text(ticketDetail['id']);
+                    codeAddress(ticketDetail['departure'], ticketDetail['arrival']);
                 }
+            }
+
+            function initialize() {
+                let options = {
+                    zoom: 16,
+                    center: new google.maps.LatLng(startLatLng[0], startLatLng[1]),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map(document.getElementById('map_canvas'), options);
+                let rendererOptions = {
+                    map: map, // Bản đồ đích
+                    draggable: true, //Cho phép kéo map
+                    preserveViewport: true
+                };
+                let directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+                let directionsService = new google.maps.DirectionsService();
+                directionsDisplay.setMap(map);
+                let request = {
+                    origin: new google.maps.LatLng(startLatLng[0], startLatLng[1]), //Điểm Start
+                    destination: new google.maps.LatLng(targetLatLng[0], targetLatLng[1]), // Điểm Đích
+                    travelMode: google.maps.DirectionsTravelMode.WALKING, // Phương tiện giao thông
+                };
+                directionsService.route(request, function (response, status) {
+                    if (status === google.maps.DirectionsStatus.OK) {
+                        new google.maps.DirectionsRenderer({
+                            map: map,
+                            directions: response,
+                            suppressMarkers: true // Xóa marker default
+                        });
+                        let leg = response.routes[0].legs[0];
+                        makeMarker(leg.end_location, markers.goalMarker, map);
+                        setTimeout(function () {
+                            map.setZoom(16); // Thay đổi tỉ lệ zoom
+                        });
+                    }
+                });
+            }
+
+            function makeMarker(position, icon, map) {
+                let marker = new google.maps.Marker({
+                    position: position,
+                    map: map,
+                    icon: icon
+                });
+            }
+
+            let markers = {
+                goalMarker: new google.maps.MarkerImage(
+                    goalMarkerImg, //  Đường dẫn của img
+                    new google.maps.Size(24, 33), // width,height của marker
+                    new google.maps.Point(0, 0),
+                    new google.maps.Point(12, 33),
+                    new google.maps.Size(24, 33))
+            };
+
+            function codeAddress(departure, arrival) {
+
+                let geocoder = new google.maps.Geocoder();
+                geocoder.geocode({'address': departure}, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        startLatLng = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
+                        geocoder.geocode({'address': arrival}, function (resultss, statuss) {
+                            if (statuss == google.maps.GeocoderStatus.OK) {
+                                targetLatLng = [resultss[0].geometry.location.lat(), resultss[0].geometry.location.lng()]
+                                initialize();
+                            } else {
+                                alert("Geocode was not successful for the following reason: " + status);
+                            }
+                        });
+                    } else {
+                        alert("Geocode was not successful for the following reason: " + status);
+                    }
+                });
             }
         });
     </script>
