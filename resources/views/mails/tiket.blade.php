b1f5fe2815c24126d71c14253ffbb415c8841b0c
truongnt01 <truongzeeeeee@gmail.com>
2023-11-11 09:24:55 +0700

    11/11 fix logout admin

--- resources/views/mails/tiket.blade.php
+++ resources/views/mails/tiket.blade.php
@@ -1,36 +1,25 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-   <title>Car Finder Pro</title>
-   <style>
-      body {
-         font-family: Arial, sans-serif;
-         background-color: #f2f2f2;
-         padding: 20px;
-      }
+    <style>
+        /* Basic styling for the email */
+        body {
+            font-family: Arial, sans-serif;
+        }
 
-      h3 {
-         color: #333333;
-      }
-
-      .container {
-         display: flex;
-         flex-direction: column;
-         align-items: center;
-         background-color: #ffffff;
-         padding: 20px;
-         border-radius: 5px;
-         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-      }
-
-      .info {
+        .info {
          display: flex;
          flex-direction: column;
          margin-top: 20px;
       }
-
-      .info-item {
+        .email-container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #e0e0e0;
+            background-color: #f8f8f8;
+        }
+        .info-item {
          margin-bottom: 10px;
       }
 
@@ -43,38 +32,56 @@
          text-align: center;
          color: #999999;
       }
-   </style>
+        h3 {
+            color: #333;
+        }
+
+        /* Style the flex container */
+        .flex-container {
+            display: flex;
+            flex-direction: column;
+        }
+
+        /* Style the data display */
+        .data {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+        }
+    </style>
 </head>
-
 <body>
+<div class="email-container">
+    <h3>CAR FINDER PRO</h3>
+    <div class="flex-container">
+        <div class="data">
+         <h3>Thông báo đơn đặt vé xe:</h3>
 
-   <div class="container">
-      <h3>Thông báo đơn đặt vé xe:</h3>
-
-      <div class="info">
-         <div class="info-item">
-            <span class="info-label">Người đặt:</span> {{$data->username}}
+         <div class="info">
+            <div class="info-item">
+               <span class="info-label">Người đặt:</span> {{$data->username}}
+            </div>
+            <div class="info-item">
+               <span class="info-label">Điểm đón:</span> {{$data->departure}}
+            </div>
+            <div class="info-item">
+               <span class="info-label">Điểm trả:</span> {{$data->arrival}}
+            </div>
+            <div class="info-item">
+               <span class="info-label">Số lượng vé:</span> {{$data->quantity}}
+            </div>
+            <div class="info-item">
+               <span class="info-label">Tổng tiền:</span> {{$data->total_price}} vnđ
+            </div>
+            <div class="info-item">
+               <span class="info-label">Hình thức thanh toán:</span> {{$data->payment_method}}
+            </div>
          </div>
-         <div class="info-item">
-            <span class="info-label">Điểm đón:</span> {{$data->departure}}
-         </div>
-         <div class="info-item">
-            <span class="info-label">Điểm trả:</span> {{$data->arrival}}
-         </div>
-         <div class="info-item">
-            <span class="info-label">Số lượng vé:</span> {{$data->quantity}}
-         </div>
-         <div class="info-item">
-            <span class="info-label">Tổng tiền:</span> {{$data->total_price}} vnđ
-         </div>
-         <div class="info-item">
-            <span class="info-label">Hình thức thanh toán:</span> {{$data->payment_method}}
-         </div>
-      </div>
-
-      <strong class="footer">Thanks & Regards.</strong>
-   </div>
-
+   
+         <strong class="footer">Thanks & Regards.</strong>
+        </div>
+    </div>
+</div>
 </body>
-
 </html>
+
