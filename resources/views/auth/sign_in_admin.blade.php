1af50c0efce91754de56f57c26d450141ff3374f
duong8888 <duongna16032003@gmail.com>
2023-12-17 20:13:53 +0700

    update

--- resources/views/auth/sign_in_admin.blade.php
+++ resources/views/auth/sign_in_admin.blade.php
@@ -5,14 +5,14 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>index-1 | Jobcy - Admin & Dashboard Template</title>
+    <title>Đăng nhập</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta content="Tailwind Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="" name="Themesbrand" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{asset('')}}client/images/favicon.ico" />
+    <link rel="shortcut icon" href="{{asset('images/icon-dev-01.png')}}" type="image/x-icon">
 
 
 
