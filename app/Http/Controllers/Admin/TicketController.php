9dab787eea572eca14691fa4b05dc53746e44e73
duong8888 <duongna16032003@gmail.com>
2024-01-07 17:30:11 +0700

    update

--- app/Http/Controllers/Admin/TicketController.php
+++ app/Http/Controllers/Admin/TicketController.php
@@ -17,7 +17,6 @@ use Illuminate\Http\Request;
 use App\Models\Ticket;
 use App\Models\User;
 use App\Models\WorkingTime;
-use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -181,7 +180,7 @@ class TicketController extends AdminBaseController
 
 
     public function getLayout(Request $request){
-        
+
         $passengerCars = PassengerCar::query()->where('id', $request->id)->first();
         if($passengerCars->vehicle_id != 0){
             $layout = SeatsLayout::query()->where('vehicle_id', $passengerCars->vehicle->id)->get();
